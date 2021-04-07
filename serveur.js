4194e9f4c9e28608b1a2fb9206b4a128cf91e818
christian <christian-gros@outlook.fr>
2021-04-07 16:07:32 +0200

    corrections

--- serveur.js
+++ serveur.js
@@ -1,16 +1,16 @@
-const net = require('net')
+const net = require("net");
 
-const server = net.createServer()
+const server = net.createServer();
 
-server.on('connection', function (socket) {
-  console.log('new client')
+server.on("connection", function (socket) {
+  console.log("Fichier XML:");
 
-  socket.on('data', function (d) {
-    console.log('data: %s', d)
-    socket.write('hello' + d);
-  })
-})
+  socket.on("data", function (d) {
+    console.log("%s", d);
+    // socket.write('hello' + d);
+  });
+});
 
 server.listen(9000, function () {
-  console.log('server listening to truc')
-})
+  console.log("server listening to truc");
+});
