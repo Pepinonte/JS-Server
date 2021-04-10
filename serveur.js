dfef9315192012f589c307b3c16b137132bc77e7
christian <christian-gros@outlook.fr>
2021-04-10 22:27:53 +0200

    split des donnees et enregisterment dans un array

--- serveur.js
+++ serveur.js
@@ -3,23 +3,34 @@ const net = require("net");
 const server = net.createServer();
 
 let arr = [];
+let i = 0;
+let parseData = [];
 
 server.on("connection", function (socket) {
   console.log("Fichier XML:");
 
   socket.on("data", (d) => {
-    let i = 0;
     arr.push(d);
     const e = arr[i].toString();
 
-    i++;
-
     const tailleArr = arr.length;
     const tailleE = e.length;
-    const dataSplit = e.split(".");
+    const dataSplitDate = e.split(".");
+    const dataSplitHeure = e.split(":");
 
+    if ((i === 0) | (i === 2)) {
+      parseData.push(dataSplitDate[0]);
+      parseData.push(dataSplitDate[1]);
+      parseData.push(dataSplitDate[2]);
+    } else if ((i === 1) | (i == 3)) {
+      parseData.push(dataSplitHeure[0]);
+      parseData.push(dataSplitHeure[1]);
+      parseData.push(dataSplitHeure[2]);
+    }
+
+    console.log(parseData);
     // socket.write('hello' + d);
-    // console.log(dataSplit[0])
+    i++;
   });
 });
 
