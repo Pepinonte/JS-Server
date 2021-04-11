c1febcfcb764c302d8436163b06f4c4da61539bb
Headhtr <christian-gros@outlook.fr>
2021-04-11 19:06:21 +0200

    correction du parsage

--- serveur.js
+++ serveur.js
@@ -4,32 +4,32 @@ const server = net.createServer();
 
 let arr = [];
 let i = 0;
-let parseData = [];
+let e = [];
+let finalParse = [];
 
 server.on("connection", function (socket) {
   console.log("Fichier XML:");
 
   socket.on("data", (d) => {
     arr.push(d);
-    const e = arr[i].toString();
+    e.push(arr[i].toString());
+    const p1 = e.toString().split(" ");
+    const p2 = p1.toString().split(".");
+    const p3 = p2.toString().split(",");
+    const p4 = p3.toString().split(":");
+    const p5 = p4.toString().split(",");
 
-    const tailleArr = arr.length;
-    const tailleE = e.length;
-    const dataSplitDate = e.split(".");
-    const dataSplitHeure = e.split(":");
-
-    if ((i === 0) | (i === 2)) {
-      parseData.push(dataSplitDate[0]);
-      parseData.push(dataSplitDate[1]);
-      parseData.push(dataSplitDate[2]);
-    } else if ((i === 1) | (i == 3)) {
-      parseData.push(dataSplitHeure[0]);
-      parseData.push(dataSplitHeure[1]);
-      parseData.push(dataSplitHeure[2]);
+    if (i == 5) {
+      p5.pop();
+      p5.pop();
+      p5.pop();
+      p5.splice(1, 1);
+      p5.splice(2, 1);
+      finalParse = p5;
+      console.log(e);
+      console.log(finalParse);
     }
-
-    console.log(parseData);
-    // socket.write('hello' + d);
+    // // socket.write('hello' + d);
     i++;
   });
 });
